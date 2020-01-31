ca85fc2dcd3db76b507c253e21e4103707d5fed7
Asokumar Gurusamy <agurusamy@paypal.com>
2020-01-31 15:27:44 -0800

    name change

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,9 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Greetings!</h1>
+    <p>Welcome to my personal page</p>
+    <p>The site is under construction</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
