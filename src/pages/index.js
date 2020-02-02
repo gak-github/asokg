a25e743fd9297a91343a70ddc442e3a054aee749
Asokumar Gurusamy <agurusamy@paypal.com>
2020-02-02 11:11:11 -0800

    react-bootstrap and custom image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,29 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Row, Col, Container, ListGroup } from "react-bootstrap"
+import Image from '../components/image'
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+  <Layout pageInfo={{ pageName: "index" }}>
+    <SEO title="Home" keywords={[`gatsby`, `react`, `bootstrap`]} />
+    <Container className="text-center">
+      <Row>
+        <Col>
+          <h3>
+              This page is under development using gatsby, react, react-bootstrap, and scss. This site is hosted on Netlify
+          </h3>
+        </Col>
+      </Row>
+      <Row className="justify-content-center my-3">
+        <Col md="6">
+          <ListGroup>
+            <Image />
+          </ListGroup>
+        </Col>
+      </Row>
+    </Container>
   </Layout>
 )
 
