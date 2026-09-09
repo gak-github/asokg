397702a048048fd747c7c27803db75b0cc3bbf2c
Asokumar Gurusamy <gak_iitm@yahoo.com>
2026-09-09 16:42:32 -0700

    ignore .next directory

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"I67+5VhsRcs6YrZRHif/L7MicNRwU8OR9RmSYPdFd30=\"\n}"
--- .next/dev/static/chunks/src_pages_index_0du2_q-._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/node_modules_next_dist_compiled_0kw0nba._.js","static/chunks/node_modules_next_dist_shared_lib_1eks-2g._.js","static/chunks/node_modules_next_dist_client_0zz_ohu._.js","static/chunks/node_modules_next_dist_0jxatvg._.js","static/chunks/node_modules_next_16hymie._.js","static/chunks/node_modules_react-dom_cjs_react-dom-client_development_1nq9a-7.js","static/chunks/node_modules_react-dom_cjs_react-dom_development_0d3scg3.js","static/chunks/node_modules_react-dom_13voigl._.js","static/chunks/node_modules_17l50y6._.js","static/chunks/[root-of-the-server]__11l-uzb._.js"],
-    source: "entry"
-});
